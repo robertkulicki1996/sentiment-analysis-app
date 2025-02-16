940e0164de9df5e1618b59439e35d12bd99edc73
Robert Kulicki <robert.kulicki@cybergeeks.pl>
2025-02-16 12:29:38 +0100

    Small fix for husky

--- src/hooks/useLanguage.tsx
+++ src/hooks/useLanguage.tsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { LanguageContext } from '../contexts/LanguageContext';
+import { useContext } from "react";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
